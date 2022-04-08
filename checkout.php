1a76e352864190536f0fa249b2797d025d3f2d60
Nehal Lakhani <nehal.l@utkdemo.com>
2022-04-08 11:52:45 +0530

    new structure

--- checkout.php
+++ checkout.php
@@ -3,7 +3,9 @@ echo "your new branch is student-management2";
 echo "one";
 echo "two";
 echo "three";
+
 ?>
 <html>
   <input type="text" name="firstname" placeholder="enter your firstname">
+  <h2>home</h2>
 </html>
