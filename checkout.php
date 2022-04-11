3dc5226e1d0d407bb7d1827a1ff2b7d8c1ef993e
Nehal Lakhani <nehal.l@utkdemo.com>
2022-04-11 09:51:44 +0530

    new 3rd change

--- checkout.php
+++ checkout.php
@@ -6,6 +6,7 @@ echo "three";
 ?>
 <html>
   <input type="text" name="firstname" placeholder="enter your firstname">
+  <input type="text" name="lastname" placeholder="enter your lastname">
   <h2>home</h2>
   <h4>New</h4>
   <h5>Another home page</h5>
