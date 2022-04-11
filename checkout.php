435de436323981c0da3fc9295cb1d9c7588d3090
Nehal Lakhani <nehal.l@utkdemo.com>
2022-04-11 09:58:44 +0530

    neww

--- checkout.php
+++ checkout.php
@@ -8,6 +8,7 @@ echo "three";
   <input type="text" name="firstname" placeholder="enter your firstname">
   <input type="text" name="lastname" placeholder="enter your lastname">
   <input type="email" name="email" placeholder="enter your email id">
+  <input type="text" name="phoneno" placeholder="enter your phone no">
   <h2>home</h2>
   <h4>New</h4>
   <h5>Another home page</h5>
